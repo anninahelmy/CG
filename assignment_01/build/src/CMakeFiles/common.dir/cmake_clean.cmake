408ac0a03105d829cc1ad35f0e868d6155ed55bc
Annina Helmy <annina.helmy@students.unibe.ch>
2020-09-29 13:32:28 +0200

    init

---
+++ assignment_01/build/src/CMakeFiles/common.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/common.dir/Cylinder.cpp.o"
+  "CMakeFiles/common.dir/Mesh.cpp.o"
+  "CMakeFiles/common.dir/Plane.cpp.o"
+  "CMakeFiles/common.dir/Scene.cpp.o"
+  "CMakeFiles/common.dir/Sphere.cpp.o"
+  "CMakeFiles/common.dir/vec3.cpp.o"
+  "libcommon.a"
+  "libcommon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/common.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ assignment_01/build/src/CMakeFiles/debug_aabb.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../debug_aabb"
+  "../debug_aabb.pdb"
+  "CMakeFiles/debug_aabb.dir/debug_aabb.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/debug_aabb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
