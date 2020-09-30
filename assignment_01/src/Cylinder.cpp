3806417b2713a82d3ac31121c0c2357a4b2f7d4e
Stern5497 <62178719+Stern5497@users.noreply.github.com>
2020-09-30 09:17:46 +0200

    - added gitignore file for build and cmake-build-debug folders - started exercise for assignment1 in Plane.cpp (almost finished) and Cylinder.cpp (equations needed)

--- assignment_01/src/Cylinder.cpp
+++ assignment_01/src/Cylinder.cpp
@@ -25,13 +25,25 @@ intersect(const Ray&  _ray,
           vec3&       _intersection_normal,
           double&     _intersection_t) const
 {
-    /** \todo
-     * - compute the first valid intersection `_ray` with the cylinder
-     *   (valid means in front of the viewer: t > 0)
-     * - store intersection point in `_intersection_point`
-     * - store ray parameter in `_intersection_t`
-     * - store normal at _intersection_point in `_intersection_normal`.
-     * - return whether there is an intersection with t > 0
-    */
-    return false;
+
+    const vec3 &dir = _ray.direction;
+    const vec3   oc = _ray.origin - center;
+
+    std::array<double, 2> t;
+    //todo size_t nsol = solveQuadratic(a,b,c,t);
+
+    _intersection_t = NO_INTERSECTION;
+
+    // Find the closest valid solution (in front of the viewer)
+    for (size_t i = 0; i < nsol; ++i) {
+        if (t[i] > 0) _intersection_t = std::min(_intersection_t, t[i]);
+    }
+
+    if (_intersection_t == NO_INTERSECTION) return false;
+
+    _intersection_point  = _ray(_intersection_t);
+    //todo _intersection_normal = (_intersection_point +- achsenabschnitt - center) / radius;
+
+    return true;
+
 }
--- assignment_01/src/Plane.cpp
+++ assignment_01/src/Plane.cpp
@@ -36,14 +36,26 @@ intersect(const Ray& _ray,
           double&    _intersection_t ) const
 {
 
-/** \todo
- * - compute the intersection of the plane with `_ray`
- * - if ray and plane are parallel there is no intersection
- * - otherwise compute intersection data and store it in `_intersection_point`, `_intersection_normal`, and `_intersection_t`.
- * - return whether there is an intersection in front of the viewer (t > 0)
-*/
+    const vec3 &dir = _ray.direction;
+    const vec3 oc = _ray.origin;
 
-    return false;
+    _intersection_t = NO_INTERSECTION;
+
+
+    if (dot(&dir, normal) == 0) return false;
+    else {
+        _intersection_t = (center - dot(normal, oc))/(dot(normal, dir));
+        if (_intersection_t<0){
+            _intersection_t = NO_INTERSECTION;
+        }
+    }
+
+    if(_intersection_t == NO_INTERSECTION) return false;
+
+    _intersection_point = _ray(_intersection_t);
+    _intersection_normal = normal;
+
+    return true;
 }
 
 
