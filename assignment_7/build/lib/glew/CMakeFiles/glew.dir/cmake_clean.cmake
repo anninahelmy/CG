ddccc617903b0fdfc9aea253a460df09bcd8e690
Annina Helmy <annina.helmy@students.unibe.ch>
2020-11-11 14:53:59 +0100

    assignment 7

---
+++ assignment_7/build/lib/glew/CMakeFiles/glew.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../libglew.a"
+  "../../libglew.pdb"
+  "CMakeFiles/glew.dir/glew.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/glew.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ assignment_7/build/src/CMakeFiles/ShadowViewer.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/annina/CG/CG/assignment_7/src/Mesh.cpp" "/Users/annina/CG/CG/assignment_7/build/src/CMakeFiles/ShadowViewer.dir/Mesh.cpp.o"
+  "/Users/annina/CG/CG/assignment_7/src/ShadowViewer.cpp" "/Users/annina/CG/CG/assignment_7/build/src/CMakeFiles/ShadowViewer.dir/ShadowViewer.cpp.o"
+  "/Users/annina/CG/CG/assignment_7/src/glfw_window.cpp" "/Users/annina/CG/CG/assignment_7/build/src/CMakeFiles/ShadowViewer.dir/glfw_window.cpp.o"
+  "/Users/annina/CG/CG/assignment_7/src/glmath.cpp" "/Users/annina/CG/CG/assignment_7/build/src/CMakeFiles/ShadowViewer.dir/glmath.cpp.o"
+  "/Users/annina/CG/CG/assignment_7/src/main.cpp" "/Users/annina/CG/CG/assignment_7/build/src/CMakeFiles/ShadowViewer.dir/main.cpp.o"
+  "/Users/annina/CG/CG/assignment_7/src/shader.cpp" "/Users/annina/CG/CG/assignment_7/build/src/CMakeFiles/ShadowViewer.dir/shader.cpp.o"
+  "/Users/annina/CG/CG/assignment_7/src/sphere.cpp" "/Users/annina/CG/CG/assignment_7/build/src/CMakeFiles/ShadowViewer.dir/sphere.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  "MESH_PATH=\"/Users/annina/CG/CG/assignment_7/meshes\""
+  "SHADER_PATH=\"/Users/annina/CG/CG/assignment_7/shaders\""
+  "TEXTURE_PATH=\"/Users/annina/CG/CG/assignment_7/textures\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/glfw/include"
+  "../src"
+  "../lib/lodePNG"
+  "../lib/glew"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/annina/CG/CG/assignment_7/build/lib/lodePNG/CMakeFiles/lodePNG.dir/DependInfo.cmake"
+  "/Users/annina/CG/CG/assignment_7/build/lib/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/Users/annina/CG/CG/assignment_7/build/lib/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
